bf1a982ed7c942983d42b8e6e55609420b7ed9de
AlanFor301 <allencc26@gmail.com>
2017-03-24 00:49:54 -0600

    set time out and output dir and file name

--- test.js
+++ test.js
@@ -1,1 +1,22 @@
 console.log('hey sup');
+
+/*normal case: counting time
+ */
+setTimeout(function(){
+  console.log('3 seconds have passed');
+}, 3000);
+
+var time = 0;
+var timer = setInterval(function(){
+  time +=2;
+  console.log(time + ' seconds have passed');
+
+/*set a terminal state
+ */
+  if (time > 5){
+    clearInterval(timer);
+  }
+}, 2000);
+
+console.log(__dirname);
+console.log(__filename);
