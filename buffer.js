20523b24c16878b6de70b1e43b1119d783ac12c2
AlanFor301 <allencc26@gmail.com>
2017-03-28 00:49:00 -0600

    edit buffer format to utf8

--- buffer.js
+++ buffer.js
@@ -1,9 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/readMe.txt');
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
 
 myReadStream.on('data', function(chunck){
   console.log('new chunck received: ');
   console.log(chunck);
-})
+});
