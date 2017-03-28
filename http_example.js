f6ed6900bc214bc01f91435693779ed3ee97a36c
AlanFor301 <allencc26@gmail.com>
2017-03-28 17:40:55 -0600

    server side res 200 with a index.html file

--- http_example.js
+++ http_example.js
@@ -1,8 +1,11 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res){
-  res.writeHead(200, {'content-Type':'text/plain'});
-  res.end('hello on html')
+  console.log('request was made: '+ req.url);
+  res.writeHead(200, {'content-Type':'text/html'});
+  var myReadStream = fs.createReadStream(__dirname+ '/index.html', 'utf8');
+  myReadStream.pipe(res);
 });
 
 server.listen(3000, '127.0.0.1');
