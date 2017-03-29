62e083c67ecd74919a3ddfe357662300fd6888e9
AlanFor301 <allencc26@gmail.com>
2017-03-29 15:42:19 -0600

    adding 404 page

--- routing.js
+++ routing.js
@@ -16,6 +16,9 @@ var server = http.createServer(function(req, res){
     // request for a end point api.
     res.writeHead(200, {'content-Type': 'application/json'});
     res.end(JSON.stringify(alan));
+  }else{
+  res.writeHead(404, {'content-Type': 'text/html'});
+  fs.createReadStream(__dirname+ '/404.html').pipe(res);
   }
 });
 
