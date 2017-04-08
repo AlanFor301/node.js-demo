3ea75fa8f886d59b8df23d948a4ce5e7f6e7e34d
AlanFor301 <allencc26@gmail.com>
2017-04-08 16:23:28 -0600

    change directory

---
+++ Java Review/FinalExample.java
@@ -0,0 +1,13 @@
+public class FinalExample{
+  public static void main(String arg[]){
+    final int digit = 10;
+    try{
+      //digit += 1;
+      System.out.println("final type cannot be changed; otherwise it will occour compiling error.");
+    }catch(Exception e){
+      System.out.println(e);
+    }finally{
+      System.out.println("digit cant be changed.");
+    }
+  }
+}
